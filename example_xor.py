softmax fix

example_xor.py
@@ -3,7 +3,7 @@ import numpy as np
 from network import Network
 
 # training data
-x_train = np.array([[0,0], [0,1], [1,0], [1,1]]) # TODO: validation set vuoto!
+x_train = np.array([[0,0], [0,1], [1,0], [1,1]])
 y_train = np.array([0, 1, 1, 0])
 
 print(x_train.shape)

layer.py
@@ -91,8 +91,8 @@ class Layer():
     def backward_propagation(self, error): # REVIEW: rename error --> delta_j
         delta = np.dot(error, self.activation_prime(self.net))
         sum_w_delta = np.dot(delta, np.transpose(self.weights)) # REVIEW: rename sum_w_delta --> delta_i
-        #weights_error = np.dot(np.transpose(self.input), delta) # dE/dW
-        weights_error = np.outer(self.input, delta) # REVIEW: rename weights_error --> delta_w
+        weights_error = np.dot(np.transpose(self.input), delta) # dE/dW
+        #weights_error = np.outer(self.input, delta) # REVIEW: rename weights_error --> delta_w
         # dBias = delta
         #accumalte deltas
         self.deltas_weights += weights_error

utils.py
@@ -54,11 +54,11 @@ def softplus_prime(x):
 def softmax(x):
     #e = np.exp(x - np.max(x, axis=1)) # TODO: normalization?
     #return e / np.sum(e, axis=1)
-    return np.exp(x) / sum(np.exp(x))
+    return np.exp(x) / np.sum(np.exp(x))
 
-def softmax_prime(x):
+def softmax_prime(x): # TODO: softmax può essere usata con numero unità output layer > 1
     f = softmax(x) 
-    return np.diagflat(f) - np.dot(f, np.transpose(f))
+    return np.diagflat(f) - np.dot(np.transpose(f), f)
 
 def logloss(x):
     # TODO: