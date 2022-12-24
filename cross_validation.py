0a5ba2bd89a468f029df8232de352ebdd7839fc6
lorenzoleuzzi1 <106617377+lorenzoleuzzi1@users.noreply.github.com>
2022-12-24 11:52:12 +0100

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -9,7 +9,7 @@ def cross_validation(network, X_train, y_train, X_test, y_test, k_out, k_inn, ne
     #     print('Number of folds k must be more than 1')
     #     exit()
     if nested: resampling = "outer"
-    else: resampling = "    inner"
+    else: resampling = "inner"
     #print("Total samples = {}".format(len(X_train)))
     skf = StratifiedKFold(n_splits=k_out, shuffle=True) #REVIEW: non divide total samples / k StratifiedGroupKFold
 
