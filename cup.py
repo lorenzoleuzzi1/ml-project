3c6c934274afad1f777a86a0cfb141a1c58196a9
lorenzoleuzzi1 <leuzzi.lorenzo1@gmail.com>
2023-01-06 02:10:38 +0100

    time and grid minor changes

--- cup.py
+++ cup.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from utils import error_plot, accuracy_plot
+from utils import error_plot, accuracy_plot, mee, mse
 from sklearn.model_selection import train_test_split
 from network import Network
 import matplotlib.pyplot as plt
+import time
 
 CUP_TRAIN_PATH = './datasets/ML-CUP22-TR.csv'
 CUP_TEST_PATH = './datasets/ML-CUP22-TS.csv'
@@ -29,63 +30,72 @@ X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=
 net = Network(
     activation_out='identity',
     classification=False,
+    hidden_layer_sizes=[100],
     activation_hidden='tanh',
     loss='mse',
-    epochs=100,
-    batch_size=32, 
+    epochs=500,
+    batch_size=256, 
     learning_rate = "fixed",
-    learning_rate_init=0.001,
-    nesterov=False,
+    learning_rate_init=0.01,
+    nesterov=False, 
     stopping_patience=10,
-    early_stopping=False
+    early_stopping=True,
+    evaluation_metric="mee",
+    validation_frequency=1
     )
+
+start = time.time()
 net.fit(X_train, y_train)
+end = time.time()
+
+
 pred = net.predict(X_test)
+print(mee(y_test, pred))
+print(end - start)
+# plt.scatter(y_test[:,0], pred[:, 0])
+# plt.xlabel("True y1")
+# plt.ylabel("Predicted y1")
+# xpoints = ypoints = plt.xlim()
+# plt.plot(xpoints, ypoints)
+# plt.show()
 
-plt.scatter(y_test[:,0], pred[:, 0])
-plt.xlabel("True y1")
-plt.ylabel("Predicted y1")
-xpoints = ypoints = plt.xlim()
-plt.plot(xpoints, ypoints)
-plt.show()
+# plt.scatter(y_test[:,0], y_test[:,0]-pred[:, 0])
+# plt.xlabel("True y1")
+# plt.ylabel("True y1 - Pred y1")
+# plt.axhline(y=0)
+# plt.show()
 
-plt.scatter(y_test[:,0], y_test[:,0]-pred[:, 0])
-plt.xlabel("True y1")
-plt.ylabel("True y1 - Pred y1")
-plt.axhline(y=0)
-plt.show()
+# plt.scatter(y_test[:,1], pred[:, 1])
+# plt.xlabel("True y2")
+# plt.ylabel("Predicted y2")
+# xpoints = ypoints = plt.xlim()
+# plt.plot(xpoints, ypoints)
+# plt.show()
 
-plt.scatter(y_test[:,1], pred[:, 1])
-plt.xlabel("True y2")
-plt.ylabel("Predicted y2")
-xpoints = ypoints = plt.xlim()
-plt.plot(xpoints, ypoints)
-plt.show()
+# plt.scatter(y_test[:,1], y_test[:,1]-pred[:, 1])
+# plt.xlabel("True y2")
+# plt.ylabel("True y2 - Pred y2")
+# plt.axhline(y=0)
+# plt.show()
 
-plt.scatter(y_test[:,1], y_test[:,1]-pred[:, 1])
-plt.xlabel("True y2")
-plt.ylabel("True y2 - Pred y2")
-plt.axhline(y=0)
-plt.show()
+# fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(5, 3))
+# for i in range(10):
+#     axes[0].scatter(y_test[i,0], pred[i, 0])
+#     axes[0].annotate(str(i), (y_test[i,0], pred[i, 0]))
+#     xpoints = ypoints = axes[0].get_xlim()
+#     axes[0].plot(xpoints, ypoints)
+#     axes[0].set_xlabel("True y1")
+#     axes[0].set_ylabel("Predicted y1")
+#     axes[1].scatter(y_test[i,1], pred[i, 1])
+#     axes[1].annotate(str(i), (y_test[i, 1], pred[i, 1]))
+#     xpoints = ypoints = axes[1].get_xlim()
+#     axes[1].plot(xpoints, ypoints)
+#     axes[1].set_xlabel("True y2")
+#     axes[1].set_ylabel("Predicted y2")
+# plt.show()
 
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(5, 3))
-for i in range(10):
-    axes[0].scatter(y_test[i,0], pred[i, 0])
-    axes[0].annotate(str(i), (y_test[i,0], pred[i, 0]))
-    xpoints = ypoints = axes[0].get_xlim()
-    axes[0].plot(xpoints, ypoints)
-    axes[0].set_xlabel("True y1")
-    axes[0].set_ylabel("Predicted y1")
-    axes[1].scatter(y_test[i,1], pred[i, 1])
-    axes[1].annotate(str(i), (y_test[i, 1], pred[i, 1]))
-    xpoints = ypoints = axes[1].get_xlim()
-    axes[1].plot(xpoints, ypoints)
-    axes[1].set_xlabel("True y2")
-    axes[1].set_ylabel("Predicted y2")
-plt.show()
-
-#print(accuracy(y_pred=pred, y_true=y_test))
-#plt.plot(net.train_losses, label="training loss", color="blue")
-#plt.plot(net.train_scores, label="training score", color="green")
-#plt.plot(net.val_losses, label="training loss", color="red")
-#plt.show()
+# #print(accuracy(y_pred=pred, y_true=y_test))
+# #plt.plot(net.train_losses, label="training loss", color="blue")
+# #plt.plot(net.train_scores, label="training score", color="green")
+# #plt.plot(net.val_losses, label="training loss", color="red")
+# #plt.show()
--- grid_search.py
+++ grid_search.py
@@ -5,19 +5,45 @@ grid = ParameterGrid([
     {
         'activation_out': ['identity'],
         'classification' : [False],
-        'activation_hidden': ['tanh', 'logistic', 'relu'], #identity?
-        'hidden_layer_sizes': [[10], [10, 10], [50], [100, ], [50, 50], [100, 100]], 
+        'activation_hidden': ['tanh'], #identity?
+        'hidden_layer_sizes': [[20], [100], [30,30], [100,100]], 
         'loss': ['mse'],
         'evaluation_metric' : ['mee'],
-        'epochs': [500, 1000],
+        'epochs': [100, 500],
         'tau' : [200], # after tau epochs lr=1/10*init
         'tol' : [0.0001],
         'learning_rate': ['fixed', 'linear_decay'],
         'lambd': [0, 0.0001, 0.001, 0.01, 0.1], # rivedi dopo aver capito come scalare lamda, quando alto serve early stopping?
-        'alpha': [0.5, 0.7, 0.9],
+        'alpha': [0, 0.5, 0.9],
         'verbose': [False],
-        'nesterov' : [True, False],
-        'early_stopping': [True, False],
+        'nesterov' : [False], # in generale non sembra influire tanto (forse peggiorare)
+        'early_stopping': [False],
+        'stopping_patience': [5],
+        'validation_size': [0.1],
+        'validation_frequency': [5],
+        'random_state': [None],
+        'reinit_weights': [True],
+        'weights_dist': [None],
+        'metric_decrease_tol': [0.1/100],
+        'batch_size': [256, 1.0],
+        'learning_rate_init': [0.0005, 0.001, 0.1]
+    },
+     {
+        'activation_out': ['identity'],
+        'classification' : [False],
+        'activation_hidden': ['tanh'], #identity?
+        'hidden_layer_sizes': [[20], [100], [30,30], [100,100]], 
+        'loss': ['mse'],
+        'evaluation_metric' : ['mee'],
+        'epochs': [100, 500],
+        'tau' : [200], # after tau epochs lr=1/10*init
+        'tol' : [0.0001],
+        'learning_rate': ['fixed', 'linear_decay'],
+        'lambd': [0, 0.0001, 0.001, 0.01, 0.1], # rivedi dopo aver capito come scalare lamda, quando alto serve early stopping?
+        'alpha': [0, 0.5, 0.9],
+        'verbose': [False],
+        'nesterov' : [False], # in generale non sembra influire tanto (forse peggiorare)
+        'early_stopping': [False],
         'stopping_patience': [5],
         'validation_size': [0.1],
         'validation_frequency': [5],
@@ -27,60 +53,8 @@ grid = ParameterGrid([
         'metric_decrease_tol': [0.1/100],
         'batch_size': [1],
         'learning_rate_init': [0.0001, 0.0005, 0.001]
-    },
-    {
-        'activation_out': ['identity'],
-        'classification' : [False],
-        'activation_hidden': ['tanh', 'logistic', 'relu'], #identity?
-        'hidden_layer_sizes': [[10], [10, 10], [50], [100, ], [50, 50], [100, 100]], 
-        'loss': ['mse'],
-        'evaluation_metric' : ['mee'],
-        'epochs': [500, 1000],
-        'tau' : [200], # after tau epochs lr=1/10*init
-        'tol' : [0.0001],
-        'learning_rate': ['fixed', 'linear_decay'],
-        'lambd': [0, 0.0001, 0.001, 0.01, 0.1], # rivedi dopo aver capito come scalare lamda, quando alto serve early stopping?
-        'alpha': [0.5, 0.7, 0.9],
-        'verbose': [False],
-        'nesterov' : [True, False],
-        'early_stopping': [True, False],
-        'stopping_patience': [5],
-        'validation_size': [0.1],
-        'validation_frequency': [5],
-        'random_state': [None],
-        'reinit_weights': [True],
-        'weights_dist': [None],
-        'metric_decrease_tol': [0.1/100],
-        'batch_size': [1.0],
-        'learning_rate_init': [0.1, 0.01]
-    },
-    {
-        'activation_out': ['identity'],
-        'classification' : [False],
-        'activation_hidden': ['tanh', 'logistic', 'relu'], #identity?
-        'hidden_layer_sizes': [[10], [10, 10], [50], [100], [50, 50], [100, 100]], 
-        'loss': ['mse'],
-        'evaluation_metric' : ['mee'],
-        'epochs': [500, 1000],
-        'tau' : [200], # after tau epochs lr=1/10*init
-        'tol' : [0.0001],
-        'learning_rate': ['fixed', 'linear_decay'],
-        'lambd': [0, 0.0001, 0.001, 0.01, 0.1], # rivedi dopo aver capito come scalare lamda, quando alto serve early stopping?
-        'alpha': [0.5, 0.7, 0.9],
-        'verbose': [False],
-        'nesterov' : [True, False],# TODO togliere ????
-        'early_stopping': [True, False],
-        'stopping_patience': [5],
-        'validation_size': [0.1],
-        'validation_frequency': [5],
-        'random_state': [None],
-        'reinit_weights': [True],
-        'weights_dist': [None],
-        'metric_decrease_tol': [0.1/100],
-        'batch_size': [32, 256], #faremo 5 o 3 fold cross alidation 1500/5*4 => 1200 1500/3*2=1000, 
-        'learning_rate_init': [0.01, 0.001]
     }
-]
+    ]
 )
 
 print(len(grid))
