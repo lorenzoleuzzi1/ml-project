be14bd312d2ac6af424b8beebaa3c7f2333a6396
Irene Testa <testa.irene1509@gmail.com>
2023-01-09 10:00:35 +0100

    new data saving

--- grid_search.py
+++ grid_search.py
@@ -1,5 +1,7 @@
 from sklearn.model_selection import ParameterGrid
 from network import Network
+import pandas as pd
+from scipy.stats import rankdata
 
 grid = ParameterGrid([
     {
@@ -24,6 +26,7 @@ grid = ParameterGrid([
         'random_state': [None],
         'reinit_weights': [True],
         'weights_dist': [None],
+        'weights_bound': [None],
         'metric_decrease_tol': [0.1/100],
         'batch_size': [256, 1.0],
         'learning_rate_init': [0.0005, 0.001, 0.1]
@@ -50,6 +53,7 @@ grid = ParameterGrid([
         'random_state': [None],
         'reinit_weights': [True],
         'weights_dist': [None],
+        'weights_bound': [None],
         'metric_decrease_tol': [0.1/100],
         'batch_size': [256, 1.0],
         'learning_rate_init': [0.0005, 0.001, 0.1]
@@ -76,6 +80,7 @@ grid = ParameterGrid([
         'random_state': [None],
         'reinit_weights': [True],
         'weights_dist': [None],
+        'weights_bound': [None],
         'metric_decrease_tol': [0.1/100],
         'batch_size': [1],
         'learning_rate_init': [0.0001, 0.0005, 0.001]
@@ -84,3 +89,39 @@ grid = ParameterGrid([
 )
 
 print(len(grid))
+
+scores = {}
+scores['tr_loss_mean'] = 1
+scores['tr_loss_dev'] = 2
+scores['val_score_mean'] = 3
+scores['val_score_dev'] = 4
+
+df_params =  pd.DataFrame(columns=[])
+df_scores =  pd.DataFrame(columns=[])
+params_list = []
+for params in grid:
+    params_list.append(params)
+    df_params = pd.concat([df_params, pd.DataFrame([params])], ignore_index=True)
+    df_scores = pd.concat([df_scores, pd.DataFrame([scores])], ignore_index=True)
+    df_scores['tr_loss_rel_dev'] = df_scores['tr_loss_dev'] / df_scores['tr_loss_mean']
+    df_scores['val_score_rel_dev'] = df_scores['val_score_dev'] / df_scores['val_score_mean']
+
+df_scores['tr_loss_mean_rank'] = rankdata(df_scores['tr_loss_mean'], method='dense')
+df_scores['tr_loss_rel_dev'] = rankdata(df_scores['tr_loss_rel_dev'], method='dense')
+df_scores['val_score_mean_rank'] = rankdata(df_scores['val_score_mean'], method='dense')
+df_scores['val_score_rel_dev'] = rankdata(df_scores['val_score_rel_dev'], method='dense')
+df_scores['params'] = params_list
+
+df_scores.to_csv('scores_df.csv')
+df_params.drop(['classification', 'verbose'], axis=1) # drop also random state, reinit weights... (?)
+df_params.to_csv('params_df.csv')
+
+# nomi colonne migliori
+# itegra con validation
+# salva anche split values
+
+# CSV --> PANDAS DATAFRAME --> DICT
+read_df = pd.read_csv('scores_df.csv', sep=",")
+read_df.drop(read_df.columns[0], axis=1, inplace=True) # drop first column
+read_dict = read_df.to_dict(orient='records')
+print(read_dict[0]) # print row 0 as dict
