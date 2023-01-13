cc7e2713870bfdc4bb36113dfacf6c2339c83772
giuliaghisolfi99@gmail.com <giuliaghisolfi99@gmail.com>
2023-01-13 16:16:46 +0100

    best model plt

--- plot_best_configs_cup.py
+++ plot_best_configs_cup.py
@@ -8,16 +8,14 @@ df = read_grid_search_results("coarse_gs.csv")
 X_train, y_train = load_dev_set_cup()
 print(df)
 
-for i in range(5,10):
+for i in range(6,10):
     config = df['params'][0]
     net = Network(**config)
     net.fit(X_train, y_train)
     
-    f = plt.figure()
-    f.set_figwidth(20)
-    f.set_figheight(20)
-    f = plt.plot(net.train_losses)
-    f = plt.plot(net.train_scores)
-    f = plt.savefig("{i}_model.pdf")
-    
+    plt.figure()
+    plt.figure(figsize=(20,20))
+    plt.plot(net.train_losses)
+    plt.plot(net.train_scores)
+    plt.savefig(f"{i}_model.pdf")
     print("done!")
