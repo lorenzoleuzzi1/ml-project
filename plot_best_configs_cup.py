e7f8834316be8cdcf48213f8b906c48eccd26a69
lorenzoleuzzi1 <leuzzi.lorenzo1@gmail.com>
2023-01-13 17:07:35 +0100

    Update plot_best_configs_cup.py

--- plot_best_configs_cup.py
+++ plot_best_configs_cup.py
@@ -7,22 +7,13 @@ import matplotlib.pyplot as plt
 df = read_grid_search_results("coarse_gs.csv")
 X_train, y_train = load_dev_set_cup()
 print(df)
-
-<<<<<<< Updated upstream
-for i in range(6,10):
-    config = df['params'][0]
-    net = Network(**config)
-    net.fit(X_train, y_train)
-    
-    plt.figure()
-=======
-for i in range(5):
+best_n = 10
+for i in range(10):
+    print(f"{i}/{best_n}")
     config = df['params'][i]
     net = Network(**config)
     net.fit(X_train, y_train)
->>>>>>> Stashed changes
     plt.figure(figsize=(20,20))
     plt.plot(net.train_losses)
     plt.plot(net.train_scores)
-    plt.savefig(f"{i}_model.pdf")
-    print("done!")
+    plt.savefig(f"{i}_model.pdf") 
