32dcc00a412fe54979f8a3939febd5971babf60e
lorenzoleuzzi1 <leuzzi.lorenzo1@gmail.com>
2023-01-14 13:01:00 +0100

    fine gs plots

--- plot_best_configs_cup.py
+++ plot_best_configs_cup.py
@@ -4,7 +4,7 @@ from cup_parsing import load_dev_set_cup
 import matplotlib.pyplot as plt
 
 
-df = read_grid_search_results("coarse_gs.csv")
+df = read_grid_search_results("fine_gs_edited.csv")
 X_train, y_train = load_dev_set_cup()
 print(df)
 best_n = 10
@@ -12,8 +12,12 @@ for i in range(10):
     print(f"{i}/{best_n}")
     config = df['params'][i]
     net = Network(**config)
+    net.early_stopping = True
+    net.stopping_patience = 500
+    net.validation_size = 0.2
     net.fit(X_train, y_train)
-    plt.figure(figsize=(20,20))
-    plt.plot(net.train_losses)
-    plt.plot(net.train_scores)
-    plt.savefig(f"{i}_model.pdf") 
+    plt.figure(figsize=(30,10))
+    #plt.plot(net.train_losses, )
+    plt.semilogy(net.train_scores, color='pink')
+    plt.semilogy(net.val_scores, color='green')
+    plt.savefig(f"fine_{i}_model.pdf") 
