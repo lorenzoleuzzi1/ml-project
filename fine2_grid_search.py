037e1f53de8cc4a0445a566eff5833cbd28e5037
giuliaghisolfi99@gmail.com <giuliaghisolfi99@gmail.com>
2023-01-14 17:32:43 +0100

    fine gs2

--- fine2_grid_search.py
+++ fine2_grid_search.py
@@ -18,7 +18,57 @@ grid = ParameterGrid([
         'lambd': [0.001, 0.0005, 0.00025, 0.0001, 0.00005],
         'alpha': [0.7, 0.75, 0.8, 0.85, 0.9],
         'verbose': [False],
-        'nesterov' : [True, False],
+        'nesterov' : [False],
+        'early_stopping': [False],
+        'stopping_patience': [30],
+        'validation_size': [0.1],
+        'random_state': [None],
+        'reinit_weights': [True], 
+        'weights_dist': [None],
+        'weights_bound': [None],
+        'metric_decrease_tol': [0.00001],
+        'batch_size': [128],
+        'learning_rate_init': [0.01, 0.008, 0.006, 0.004]
+    },
+    {
+        'activation_out': ['identity'],
+        'classification' : [False],
+        'activation_hidden': ['logistic'],
+        'hidden_layer_sizes': [ [60, 30], [30, 30, 30]],
+        'loss': ['mse'],
+        'evaluation_metric' : ['mee'],
+        'epochs': [500],
+        'tol' : [0.0001],
+        'learning_rate': ['fixed'],
+        'lambd': [0.001, 0.0005, 0.00025, 0.0001, 0.00005],
+        'alpha': [0.75, 0.8, 0.85, 0.9],
+        'verbose': [False],
+        'nesterov' : [True],
+        'early_stopping': [False],
+        'stopping_patience': [30],
+        'validation_size': [0.1],
+        'random_state': [None],
+        'reinit_weights': [True], 
+        'weights_dist': [None],
+        'weights_bound': [None],
+        'metric_decrease_tol': [0.00001],
+        'batch_size': [128],
+        'learning_rate_init': [0.008, 0.006, 0.004, 0.002]
+    },
+    {
+        'activation_out': ['identity'],
+        'classification' : [False],
+        'activation_hidden': ['logistic'],
+        'hidden_layer_sizes': [ [60, 30], [30, 30, 30]],
+        'loss': ['mse'],
+        'evaluation_metric' : ['mee'],
+        'epochs': [500],
+        'tol' : [0.0001],
+        'learning_rate': ['fixed'],
+        'lambd': [0.001, 0.0005, 0.00025, 0.0001, 0.00005],
+        'alpha': [0.7],
+        'verbose': [False],
+        'nesterov' : [True],
         'early_stopping': [False],
         'stopping_patience': [30],
         'validation_size': [0.1],
@@ -44,7 +94,7 @@ grid = ParameterGrid([
         'lambd': [0.005, 0.001, 0.0005, 0.00025, 0.0001],
         'alpha': [0.6, 0.65, 0.7, 0.75, 0.8, 0.85],
         'verbose': [False],
-        'nesterov' : [True, False],
+        'nesterov' : [False, True],
         'early_stopping': [False],
         'stopping_patience': [30],
         'validation_size': [0.1],
@@ -63,27 +113,32 @@ print(len(grid))
 grid_splitted = np.array_split(grid, 9)
 
 
-"""print(grid_splitted[4][8])
+"""results_path = '/kaggle/working/fine_gs2_results_giulia1.csv'
+grid = grid_splitted[0]
 
-print(grid_splitted[5][3])
+results_path = '/kaggle/working/fine_gs2_results_giulia2.csv'
+grid = grid_splitted[1]
 
-print(grid_splitted[6][0])
+results_path = '/kaggle/working/fine_gs2_results_giulia3.csv'
+grid = grid_splitted[2]
 
-print(grid_splitted[7][9])
-print(grid_splitted[7][10])
-print(grid_splitted[7][25])
-print(grid_splitted[7][26])
-print(grid_splitted[7][41])
-print(grid_splitted[7][42])
-print(grid_splitted[7][57])
-print(grid_splitted[7][58])
-print(grid_splitted[7][73])
-print(grid_splitted[7][74])
-print(grid_splitted[7][89])
-print(grid_splitted[7][90])
-print(grid_splitted[7][105])
-print(grid_splitted[7][106])
-print(grid_splitted[7][107])"""
+results_path = '/kaggle/working/fine_gs2_results_irene1.csv'
+grid = grid_splitted[3]
 
-"""X_dev, y_dev = load_dev_set_cup()
-grid_search_cv(grid=grid, X=X_dev, y=y_dev, k=5, results_path=results_path, evaluation_metric='mse')"""
+results_path = '/kaggle/working/fine_gs2_results_irene2.csv'
+grid = grid_splitted[4]
+
+results_path = '/kaggle/working/fine_gs2_results_irene3.csv'
+grid = grid_splitted[5]
+
+results_path = '/kaggle/working/fine_gs2_results_lorenzo1.csv'
+grid = grid_splitted[6]
+
+results_path = '/kaggle/working/fine_gs2_results_lorenzo2.csv'
+grid = grid_splitted[7]
+
+results_path = '/kaggle/working/fine_gs2_results_lorenzo3.csv'
+grid = grid_splitted[8]"""
+
+X_dev, y_dev = load_dev_set_cup()
+grid_search_cv(grid=grid, X=X_dev, y=y_dev, k=5, results_path=results_path, evaluation_metric='mse')
