aad157809d4b71d421f6cec7a353d8d38a4a9c77
giuliaghisolfi99@gmail.com <giuliaghisolfi99@gmail.com>
2023-01-16 13:28:45 +0100

    final ensamble

--- k_fold_bests.py
+++ k_fold_bests.py
@@ -8,7 +8,7 @@ from cup_parsing import load_dev_set_cup
 X_dev, y_dev = load_dev_set_cup()
 df = read_grid_search_results("fine_gs2_results.csv")
 best_n = 10
-results_path = 'k_fold_bests.csv'
+results_path = '/kaggle/working/k_fold_bests.csv'
 
 df_scores = pd.DataFrame(columns=[])
 for i in range(best_n):
@@ -16,7 +16,7 @@ for i in range(best_n):
     config = df['params'][i]
     print(config)
     net = Network(**config)
-    results = k_fold_cross_validation(net, X_dev, y_dev, k=5)
+    results = k_fold_cross_validation(net, X_dev, y_dev, k=5, evaluation_metric='mse')
     results['params'] = json.dumps(config)
     df_scores = pd.concat([df_scores, pd.DataFrame([results])], ignore_index=True)
 
