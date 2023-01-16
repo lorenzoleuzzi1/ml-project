6ae25741d052d5a95a79392f1fc0c015120a1dd6
giuliaghisolfi99@gmail.com <giuliaghisolfi99@gmail.com>
2023-01-16 16:14:00 +0100

    ensemble cup + plot

--- ensemble_assesment.py
+++ ensemble_assesment.py
@@ -22,8 +22,6 @@ train_scores = []
 val_scores = []
 epochs = []
 
-fig1, _ = plt.figure() # mse
-fig2, _ = plt.figure() # mee
 
 for i in range(best_n):
     print(f"{i}/{best_n}")
@@ -34,17 +32,21 @@ for i in range(best_n):
         net = Network(**config)
         net.fit(X_train, y_train, X_test, y_test)
         if i==0 and j==0:
-            fig1.semilogy(net.train_losses, label='Development set (MSE)', color='blue', alpha=0.7, linewidth=0.5)
-            fig1.semilogy(net.val_losses, label='Internal test set (MSE)', color='red', alpha=0.7, linewidth=0.5)
+            plt.figure('mse')
+            plt.semilogy(net.train_losses, label='Development set (MSE)', color='lightsteelblue', linewidth=0.5)
+            plt.semilogy(net.val_losses, label='Internal test set (MSE)', color='ligthcoral', linewidth=0.5)
         else:
-            fig1.semilogy(net.train_losses, color='blue', alpha=0.7, linewidth=0.5)
-            fig1.semilogy(net.val_losses, color='red', alpha=0.7, linewidth=0.5)
+            plt.figure('mse')
+            plt.semilogy(net.train_losses, color='lightsteelblue', linewidth=0.5)
+            plt.semilogy(net.val_losses, color='red', linewidth=0.5)
         if i==0 and j==0:
-            fig2.semilogy(net.train_scores, label='Development set (MEE)', color='blue', alpha=0.7, linewidth=0.5)
-            fig2.semilogy(net.val_scores, label='Internal test set (MEE)', color='red', alpha=0.7, linewidth=0.5)
+            plt.figure('mee')
+            plt.semilogy(net.train_scores, label='Development set (MEE)', color='lightsteelblue', linewidth=0.5)
+            plt.semilogy(net.val_scores, label='Internal test set (MEE)', color='ligthcoral', linewidth=0.5)
         else:
-            fig2.semilogy(net.train_scores, color='blue', alpha=0.7, linewidth=0.5)
-            fig2.semilogy(net.val_scores, color='red', alpha=0.7, linewidth=0.5)
+            plt.figure('mee')
+            plt.semilogy(net.train_scores, color='lightsteelblue', linewidth=0.5)
+            plt.semilogy(net.val_scores, color='ligthcoral', linewidth=0.5)
         
         train_losses.append(net.train_losses)
         val_losses.append(net.val_losses)
@@ -52,7 +54,7 @@ for i in range(best_n):
         val_scores.append(net.val_scores)
         Y_pred = net.predict(X_test)
         preds.append(Y_pred)
-        epochs.append(net.best_epochs)
+        epochs.append(net.best_epoch)
 
 train_losses_mean = np.mean(train_losses, axis=0)
 val_losses_mean = np.mean(val_losses, axis=0)
@@ -95,22 +97,23 @@ data = {
     'val_score_mean_best_epoch': val_score_mean
 }
 
-fig1.semilogy(train_losses_mean, color='blue', label='Ensemble development set (MSE)')
-fig1.semilogy(val_losses_mean , color='red', linestyle='-', label='Ensemble internal test set (MSE)')
-fig2.semilogy(train_scores_mean, color='blue', label='Ensemble development set (MEE)')
-fig2.semilogy(val_scores_mean, color='red', linestyle='-', label='Ensemble internal test set (MEE)')
-fig1.legend()
-fig1.grid()
-fig1.xlabel('Epochs')
-fig1.ylabel('Log(Loss)')
+plt.figure('mse')
+plt.semilogy(train_losses_mean, color='blue', label='Ensemble development set (MSE)')
+plt.semilogy(val_losses_mean , color='red', linestyle='-', label='Ensemble internal test set (MSE)')
 
-fig2.legend()
-fig2.grid()
-fig1.xlabel('Epochs')
-fig1.ylabel('Log(Error)')
+plt.legend()
+plt.xlabel('Epochs')
+plt.ylabel('Log(Loss)')
+plt.savefig('mse_curves.pdf', bbox_inches="tight")
 
-fig1.savefig('mse_curves.pdf', bbox_inches="tight")
-fig1.savefig('mee_curves.pdf', bbox_inches="tight")
+plt.figure('mee')
+plt.semilogy(train_scores_mean, color='blue', label='Ensemble development set (MEE)')
+plt.semilogy(val_scores_mean, color='red', linestyle='-', label='Ensemble internal test set (MEE)')
+
+plt.legend()
+plt.xlabel('Epochs')
+plt.ylabel('Log(Error)')
+plt.savefig('mee_curves.pdf', bbox_inches="tight")
 
 mean_preds = np.mean(np.array(preds), axis=0)
 mse_test = mse(mean_preds, y_test)
@@ -120,7 +123,7 @@ data['mse_test'] = mse_test
 data['mee_test'] = mee_test
 data['mean_preds'] = mean_preds
 
-file = open('/ensemble_assesment.pkl', 'wb')
+file = open('./ensemble_assesment.pkl', 'wb')
 pickle.dump(data, file)
 file.close()
 
