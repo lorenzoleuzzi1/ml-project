5398f10fe25cf84e581859f88a73cd55db668906
lorenzoleuzzi1 <106617377+lorenzoleuzzi1@users.noreply.github.com>
2023-01-19 14:02:14 +0100

    Update best_10_models_cup.py

--- best_10_models_cup.py
+++ best_10_models_cup.py
@@ -5,7 +5,7 @@ import time
 
 configs = read_csv_results("csv_results/fine_gs2_results.csv")['params'][:10]
 X_train, y_train = load_dev_set_cup()
-X_test, y_test = load_internal_test_set_cup()
+X_test, y_test = load_internal_test_cup()
 scores = []
 for config in configs:
     #print(f"Running cup with the following configuration:\n{config}")
